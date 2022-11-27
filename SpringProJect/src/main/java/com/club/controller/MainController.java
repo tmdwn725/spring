8826d205c8429f1ff402dc2a34cbbe74f66a0cc5
user <user@DESKTOP-4Q0B43H>
2022-11-27 23:54:21 +0900

    젠킨스 테스트

--- SpringProJect/src/main/java/com/club/controller/MainController.java
+++ SpringProJect/src/main/java/com/club/controller/MainController.java
@@ -9,7 +9,6 @@ public class MainController {
 	@GetMapping("/main")
 	public String main(Model model) {
 		
-		//model.addAttribute("members",memberList);
 		//System.out.println(memberList.size());
 		return "main";
 	}
