bd0bcc22c5c3e3036cf6ece20cbeabe5f0a2e36c
user <user@DESKTOP-4Q0B43H>
2022-12-12 21:19:01 +0900

    DTO 커밋

---
+++ SpringProJect/src/main/java/com/example/demo/club/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package com.example.demo.club.dto;
+
+
+public class UserDTO {
+	private long  id;
+	private String pw;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+}
