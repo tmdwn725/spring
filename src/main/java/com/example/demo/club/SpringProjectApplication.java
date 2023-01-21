9e4a0dd14703dbd6ecd45c7b7e7bf0649b60f69b
user <user@DESKTOP-0CFH0AR>
2023-01-21 19:07:01 +0900

    pid 번호 파일 생성 commit

--- src/main/java/com/example/demo/club/SpringProjectApplication.java
+++ src/main/java/com/example/demo/club/SpringProjectApplication.java
@@ -3,20 +3,16 @@ package com.example.demo.club;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringProjectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("args:" + args);
-		/*ApplicationContext applicationContext =
-				SpringApplication.run(SpringProjectApplication.class, args);
-
-		for (String name : applicationContext.getBeanDefinitionNames()) {
-			System.out.println("springName:" + name);
-		}*/
-		SpringApplication.run(SpringProjectApplication.class, args);
+		SpringApplication application = new SpringApplication(SpringProjectApplication.class);
+	    application.addListeners(new ApplicationPidFileWriter());
+	    application.run(args);
 	}
 
 }
