0c11ae70f2430f3745e25ada47be34e4a80be0f7
PHG <rectangle714@naver.com>
2023-02-20 21:15:39 +0900

    수정 내용 커밋 20230220

--- src/main/java/com/example/demo/club/domain/Member.java
+++ src/main/java/com/example/demo/club/domain/Member.java
@@ -11,7 +11,6 @@ import java.util.List;
 @Entity
 @Getter
 @Table(name = "member")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
     @Id
---
+++ src/test/java/com/example/demo/club/MemberTest.java
@@ -0,0 +1,42 @@
+package com.example.demo.club;
+
+import com.example.demo.club.config.SecurityConfig;
+import com.example.demo.club.domain.Role;
+import com.example.demo.club.domain.Member;
+import com.example.demo.club.repository.MemberRepository;
+import com.example.demo.club.service.MemberService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@SpringBootTest
+public class MemberTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private MemberService memberService;
+
+    @Test
+    @Rollback(value = false)
+    public void createMember(){
+        String password = new SecurityConfig().getPasswordEncoder().encode("1234");
+        Member member = new Member();
+        member.createMember("phg", password, "", Role.USER);
+        memberRepository.save(member);
+    }
+
+    public void findAll(){
+        List<Member> list = memberService.findAll();
+
+        for (Member mem : list) {
+            System.out.println(mem.getMemberId());
+        }
+    }
+
+}
