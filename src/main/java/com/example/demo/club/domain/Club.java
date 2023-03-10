0070a76916205ffe31be6632df3460f5bfb41450
tmdwn752@naver.com <tmdwn752@naver.com>
2023-03-10 18:05:26 +0900

    git ignore push

--- src/main/java/com/example/demo/club/domain/Club.java
+++ src/main/java/com/example/demo/club/domain/Club.java
@@ -3,15 +3,7 @@ package com.example.demo.club.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -45,7 +37,7 @@ public class Club extends BaseEntity {
 	@OneToMany(mappedBy = "club")
 	private List<ClubInfo> clubInfoList = new ArrayList<>();
 	
-	@OneToOne
+	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="rpt_img_fl_seq")
 	private File file;
 
--- src/main/java/com/example/demo/club/domain/ClubInfo.java
+++ src/main/java/com/example/demo/club/domain/ClubInfo.java
@@ -2,14 +2,7 @@ package com.example.demo.club.domain;
 
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import org.springframework.data.annotation.CreatedDate;
 
@@ -27,11 +20,11 @@ public class ClubInfo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long clubInfoSeq;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "club_seq")
 	private Club club;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_seq")
 	private Member member;
 	
