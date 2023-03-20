ed9cc8f5e03c800a219284e707b4d9f60034e7d6
sjmoon <tmdwn752@naver.com>
2023-03-20 22:50:38 +0900

    테스트 케이스 변경_0320

--- src/test/java/com/example/demo/club/member/createTest.java
+++ src/test/java/com/example/demo/club/member/createTest.java
@@ -6,22 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import com.example.demo.club.domain.*;
+import com.example.demo.club.repository.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
-import com.example.demo.club.domain.Club;
-import com.example.demo.club.domain.ClubInfo;
-import com.example.demo.club.domain.File;
-import com.example.demo.club.domain.Member;
-import com.example.demo.club.domain.Role;
-import com.example.demo.club.domain.School;
-import com.example.demo.club.repository.ClubInfoRepository;
-import com.example.demo.club.repository.ClubRepository;
-import com.example.demo.club.repository.FileRepository;
-import com.example.demo.club.repository.MemberRepository;
-import com.example.demo.club.repository.SchoolRepository;
 import com.example.demo.club.security.SecurityConfig;
 
 import lombok.extern.slf4j.Slf4j;
@@ -45,9 +36,12 @@ public class createTest {
     @Autowired
     private FileRepository fileRepository;
 
+    @Autowired
+    private ChatRoomRepository chatRoomRepository;
+
     @Test
     @Rollback(value = false)
-    void onlyMemberCreate() {
+    public void onlyMemberCreate() {
         String password = new SecurityConfig().getPasswordEncoder().encode("1234");
         Member member = new Member();
         member.createMember("phg","박형근", password, "", Role.USER);
@@ -56,7 +50,7 @@ public class createTest {
 
     @Test
     @Rollback(value = false)
-    void onlyClubCreate() {
+    public void onlyClubCreate() {
         Optional<Member> member = memberRepository.findByMemberId("phg");
         Club club = clubRepository.findByClubNm("축구동아리");
         member.ifPresentOrElse(value -> {
@@ -69,49 +63,59 @@ public class createTest {
     @Test
     public void createMember(){
         Member mem = new Member();
-        Club club = new Club();
-        File fl = new File();
     	List<Club>clubs = new ArrayList<>();
     	ClubInfo clubInfo = new ClubInfo();
-    	List<ClubInfo>clubInfos = new ArrayList<>();
+        ChatRoom chatRoom = new ChatRoom();
+        List<ChatRoom> chatRooms = new ArrayList<>();
+    	List<ClubInfo> clubInfos = new ArrayList<>();
     	LocalDateTime currentDate = LocalDateTime.now();
     	List<File> files = new ArrayList<>();
-
+        String [][] fileAtb = {{"soccer.jpg","/images/club/soccer.jpg", "jpg"}
+                            ,{"baseball.jpg","/images/club/baseball.jpg", "jpg"}
+                            ,{null,null,null}};
+        String [][] clubAtb = {{"100101", "200101", "축구동아리","101","저희는 서울대학교를 대표하는 축구동아리 사커스입니다."}
+                            ,{"100101", "200101", "농구동아리","102","저희는 서울대학교의 대표 농구동아리 비스킷입니다."}
+                            ,{"100101", "200102", "공예동아리","103","저희는 예술을 좋아하는 동아리입니다."}};
 
         String password = new SecurityConfig().getPasswordEncoder().encode("1234");
         mem.createMember("sjmoon","문승주", password, "", Role.USER);
-       
-    	fl.createFile("soccer.jpg","/images/club/soccer.jpg", "jpg");
-    	files.add(fl);
 
-        club.createClub("100101", "200101", "축구동아리","101","저희는 서울대학교를 대표하는 축구동아리 사커스입니다.",fl);
-    	clubs.add(club);
+        for(int i = 0; i < 3; i++){
+            File file = createFile(fileAtb[i][0], fileAtb[i][1], fileAtb[i][2]);
+            if (i < 2) {
+                files.add(file);
+            }else{
+                file = null;
+            }
 
-    	clubInfo.createClubInfo(club, mem, currentDate);
-    	clubInfos.add(clubInfo);
-
-    	fl = new File();
-    	fl.createFile("baseball.jpg","/images/club/baseball.jpg", "jpg");
-    	files.add(fl);
-    	
-    	club = new Club();
-    	club.createClub("100101", "200101", "농구동아리","102","저희는 서울대학교의 대표 농구동아리 비스킷입니다.",fl);
-    	clubs.add(club);
-
-    	clubInfo = new ClubInfo();
-    	clubInfo.createClubInfo(club, mem, currentDate);
-    	clubInfos.add(clubInfo);
-
-    	club = new Club();
-    	club.createClub("100101", "200102", "공예동아리","103","저희는 예술을 좋아하는 동아리입니다.",null);
-    	clubs.add(club);
+            Club club = createClub(clubAtb[i][0], clubAtb[i][1], clubAtb[i][2],clubAtb[i][3],clubAtb[i][4],file);
+            clubs.add(club);
+            if (i < 2){
+                clubInfo = new ClubInfo();
+                clubInfo.createClubInfo(club, mem, currentDate);
+                clubInfos.add(clubInfo);
+            }
+            chatRoom = new ChatRoom();
+            chatRoom.createChatRoom(club,mem);
+            chatRooms.add(chatRoom);
+        }
 
     	fileRepository.saveAll(files);
     	memberRepository.save(mem);
         clubRepository.saveAll(clubs);
         clubinfoRepository.saveAll(clubInfos);
+        chatRoomRepository.saveAll(chatRooms);
+    }
 
-
+    File createFile(String fileNm, String filePth, String fileExt){
+        File file = new File();
+        file.createFile(fileNm,filePth, fileExt);
+        return file;
+    }
+    Club createClub(String schoolCd, String clubClsCd, String clubNm, String roomNm,String introduce,File file)  {
+        Club club = new Club();
+        club.createClub(schoolCd, clubClsCd, clubNm,roomNm,introduce,file);
+        return club;
     }
 
     @Test
