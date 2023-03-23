98095e631f48d13d0a631d3b3a74b341f3f6c87f
tmdwn752@naver.com <tmdwn752@naver.com>
2023-03-23 20:14:38 +0900

    동아리 실시간 채팅방 기능 구현_2023.03.21

--- src/main/resources/static/js/common/side.js
+++ src/main/resources/static/js/common/side.js
@@ -14,3 +14,13 @@ $(function(){
         };
     });
 });
+
+function openModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
