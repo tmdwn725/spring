0a0d4b72df5b1e94ef8af89d83f72de9341b5306
sjmoon <tmdwn752@naver.com>
2023-04-01 17:22:06 +0900

    로그인 수정_230401

--- src/main/java/com/example/demo/club/service/MemberService.java
+++ src/main/java/com/example/demo/club/service/MemberService.java
@@ -2,7 +2,12 @@ package com.example.demo.club.service;
 
 import java.util.List;
 
+import com.example.demo.club.component.JwtTokenProvider;
+import com.example.demo.club.dto.TokenInfo;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,6 +28,8 @@ public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
     private final ModelMapper modelMapper;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtTokenProvider jwtTokenProvider;
 
     /** 생성 **/
 
@@ -56,6 +63,22 @@ public class MemberService implements UserDetailsService {
                 .password(member.getPassword())
                 .roles(member.getRole().name())
                 .build();
+    }
+
+    @Transactional
+    public TokenInfo login(String memberId, String password) {
+        // 1. Login ID/PW 를 기반으로 Authentication 객체 생성
+        // 이때 authentication 는 인증 여부를 확인하는 authenticated 값이 false
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(memberId, password);
+
+        // 2. 실제 검증 (사용자 비밀번호 체크)이 이루어지는 부분
+        // authenticate 매서드가 실행될 때 CustomUserDetailsService 에서 만든 loadUserByUsername 메서드가 실행
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        // 3. 인증 정보를 기반으로 JWT 토큰 생성
+        TokenInfo tokenInfo = jwtTokenProvider.generateToken(authentication);
+
+        return tokenInfo;
     }
 
 
