d55d06e559fc5f7a4b8b251fd4b20b7e42bb50fe
sjmoon <tmdwn752@naver.com>
2023-04-09 19:41:40 +0900

    redis_추가_230409

---
+++ src/main/java/com/example/demo/club/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo.club.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+public class RedisConfig {
+    private final String redisHost;
+    private final int redisPort;
+
+    public RedisConfig(@Value("${spring.redis.host}") final String redisHost,
+                       @Value("${spring.redis.port}") final int redisPort) {
+        this.redisHost = redisHost;
+        this.redisPort = redisPort;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}
