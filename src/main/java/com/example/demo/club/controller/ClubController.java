dc533073a62d15fb9e8ec37653ab89a05b776480
sjmoon <tmdwn752@naver.com>
2023-04-26 02:39:17 +0900

    일정_관리_기능_개발_230426

--- src/main/java/com/example/demo/club/controller/ClubController.java
+++ src/main/java/com/example/demo/club/controller/ClubController.java
@@ -2,26 +2,28 @@ package com.example.demo.club.controller;
 
 import com.example.demo.club.common.CookieUtil;
 import com.example.demo.club.dto.ClubInfoDTO;
+import com.example.demo.club.dto.ScheduleDTO;
 import com.example.demo.club.security.jwt.JwtTokenProvider;
-import com.example.demo.club.service.CdService;
+import com.example.demo.club.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.club.dto.ClubDTO;
 import com.example.demo.club.dto.MemberDTO;
-import com.example.demo.club.service.ClubInfoService;
-import com.example.demo.club.service.ClubService;
-import com.example.demo.club.service.MemberService;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
 @RequestMapping("/club")
@@ -29,14 +31,12 @@ import javax.servlet.http.HttpServletRequest;
 public class ClubController {
 
 	private final ClubService clubService;
-
 	private final ClubInfoService clubInfoService;
-
 	private final MemberService memberService;
-
+	private final ScheduleService scheduleService;
 	private final CdService cdService;
-
 	private final JwtTokenProvider jwtTokenProvider;
+
 	
 	@GetMapping("/myClub")
 	public String getClub(HttpServletRequest request, Model model, ClubDTO cDTO) {
@@ -63,4 +63,10 @@ public class ClubController {
 		return ResponseEntity.ok().build();
 	}
 
+	@GetMapping("/getClubSchedule/{date}")
+	public ResponseEntity<List<ScheduleDTO>> getClubSchedule(@PathVariable(required = false) String date, Model model){
+		LocalDateTime stDt = LocalDateTime.parse(date + "T00:00:00");
+		List<ScheduleDTO> list = scheduleService.findClubScheduleList(1L,stDt,stDt.plusDays(1));
+		return new ResponseEntity<>(list, HttpStatus.OK);
+	}
 }
--- src/main/resources/static/js/club/club.js
+++ src/main/resources/static/js/club/club.js
@@ -2,3 +2,32 @@ function closeScheduleModal() {
     var modal = document.getElementById("clubScheModal");
     modal.style.display = "none";
 }
+
+function loadEvents(date) {
+  // 선택된 날짜에서 년, 월, 일을 추출
+  var year = date.getFullYear();
+  var month = ("0" + (date.getMonth() + 1)).slice(-2);
+  var day = ("0" + date.getDate()).slice(-2);
+
+  // 날짜 문자열 생성 (YYYY-MM-DD)
+  var dateString = year + "-" + month + "-" + day;
+
+  // 서버에 일정 조회 요청
+  $.ajax({
+    url: "/club/getClubSchedule/" + dateString,
+    type: "GET",
+    dataType: "json",
+    success: function(data) {
+        // 응답 데이터를 템플릿에 적용하여 리스트 만들기
+        var listItems = "";
+        data.forEach(function(item) {
+            listItems += "<li>" + item.scheduleNm + "</li>";
+        });
+        $("#list").html(listItems); // 리스트를 모달창에 뿌리기
+        $("#my-modal").modal("show"); // 모달창 열기
+    },
+  error: function(xhr, status, error) {
+    alert("데이터를 가져오는데 실패했습니다.");
+  }
+});
+}
